308c1182b79544883c69d62dc58b4f87b5f22ea1
virajpawar24 <virajpawar2021@gmail.com>
2026-01-20 11:10:08 +0530

    Time: 13 ms (36.7%), Space: 46.6 MB (18.28%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,22 +1,15 @@
 class Solution {
     public boolean isPalindrome(String s) {
         s = s.toLowerCase().replaceAll("[^a-z0-9]","");
-        int l = 0; int r = s.length() - 1;
-        while(l<r){
-           if(s.charAt(l)!=s.charAt(r)){
-            return false;
-           }
-           l++;
-           r--;
+        int left = 0;
+        int right = s.length() -1;
+        while(left <right){
+            if(s.charAt(left) !=s.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
         }
         return true;
-
-    }
-      public static void main(String[] args){
-        Solution sol = new Solution();
-        String s = "AmanaplanacanalPanama";
-        System.out.println(sol.isPalindrome(s));
-        System.out.println(sol.isPalindrome("race a car"));
-        // System.out.println("A man, a plan, a canal: Panama");
     }
 }
