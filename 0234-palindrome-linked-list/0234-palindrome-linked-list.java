8ee4a1ebc0a8caae2b8785147679a985116d6ce0
virajpawar24 <virajpawar2021@gmail.com>
2026-03-03 01:11:35 +0530

    Time: 8 ms (21.4%), Space: 99.1 MB (25.49%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        List<Integer> arr = new ArrayList<>();
+        while(head!=null){
+            arr.add(head.val);
+            head = head.next;
+
+        }
+        int left = 0;
+        int right = arr.size() -1;
+
+        while(left<right){
+            if(!arr.get(left).equals(arr.get(right))){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+     
+    }
+}
