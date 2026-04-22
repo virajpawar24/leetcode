d5dddd495b04d79a1f43ce5830999433e26c6038
virajpawar24 <virajpawar2021@gmail.com>
2026-04-22 23:20:47 +0530

    Time: 0 ms (100%), Space: 42.9 MB (14.48%) - LeetHub

---
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int removePalindromeSub(String s) {
+       
+       if(s.equals(new StringBuilder(s).reverse().toString())) return 1;
+       return 2;
+            }
+        
+    }
