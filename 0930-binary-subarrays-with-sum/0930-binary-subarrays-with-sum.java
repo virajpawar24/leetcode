aa9f25b101c8a1f395aa0eff03d625981d42660a
virajpawar24 <virajpawar2021@gmail.com>
2026-05-14 18:02:00 +0530

    Time: 1 ms (100%), Space: 50.9 MB (66.59%) - LeetHub

---
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int numSubarraysWithSum(int[] nums, int goal) {
+
+           return atMost(nums, goal) - atMost(nums, goal - 1);
+    }
+    public int atMost(int[] nums, int goal) {
+        if(goal<0) return 0;
+        int l = 0;
+        int cnt = 0;
+        int sum = 0;
+     for(int  r = 0 ; r < nums.length; r++ ){
+       sum += nums[r];
+      while(sum > goal){
+        sum -=nums[l];
+        l++;
+      }
+      cnt += (r - l + 1); 
+     }
+     return cnt;
+
+    }
+}
